d8eabf102ca32256624eff113f0bad224989165e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 13:27:25 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -47,9 +47,11 @@ output "redis" {
 }
 
 resource "aws_route53_record" "redis" {
-  name        = "redis-${var.ENV}"
+  name        = "${var.COMPONENT}.${data.aws_route53_zone.jithendar.name}"
   type        = "CNAME"
-  zone_id     = data.terraform_remote_state.vpc.outputs.ZONE_ID
-  ttl         = "1000"
-  records     = [aws_elasticache_cluster.redis.cache_nodes[0].address] //endpoint wont be der..so using cache nodes... and cache node will have id, address, port and availability_zone information
+  zone_id     = data.aws_route53_zone.jithendar.zone_id
+  ttl         = "300"
+  records     = [aws_elasticache_cluster.redis.cache_nodes[0].address] 
 }
+
+//records = [aws_elasticache_cluster.redis.cache_nodes[0].address]  endpoint wont be der..so using cache nodes... and cache node will have id, address, port and availability_zone information
