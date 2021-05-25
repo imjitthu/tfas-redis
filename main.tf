e8ccdb05c23618d8fa036c24900aa777ae711926
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 13:24:30 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -1,48 +1,55 @@
-locals {
-  key_name = "test"
-  key_path = "test.pem"
+# resource "aws_elasticache_subnet_group" "redis" {
+#   name       = "redis-${var.ENV}"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+# }
+
+resource "aws_elasticache_parameter_group" "redis" {
+  name        = "redis-cluster-pg-roboshop"
+  family      = "redis5.0"
+  description = "Redis default cluster parameter group"
 }
 
-resource "aws_instance" "redis" {
-  # aws_spot_instabce_request for spot instance
-  ami = data.aws_ami.ami.id
-  instance_type = "${var.INSTANCE_TYPE}"
-  #key_name = local.key_name
-  # spot_type = "one-time"  aws_spot_instance_request
-  tags = {
-    "Name" = "${var.COMPONENT}-Server"
-  }
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-roboshop"
+  engine               = "redis"
+  node_type            = "cache.t3.small"
+  num_cache_nodes      = 1
+  parameter_group_name = aws_elasticache_parameter_group.redis.name
+  engine_version       = "5.0.5"
+  port                 = 6379
+  #subnet_group_name    = aws_elasticache_subnet_group.redis.name
+  #security_group_ids   = [aws_security_group.allow_redis.id]
+}
+# resource "aws_security_group" "allow_redis" {
+#   name          = "allow-redis-${var.ENV}"
+#   description   = "allow-redis-${var.ENV}"
+#   vpc_id        = data.terraform_remote_state.vpc.outputs.VPC_ID
+#   ingress {
+#     description = "SSH"
+#     from_port   = 6379
+#     to_port     = 6379
+#     protocol    = "tcp"
+#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   tags          = {
+#     Name        = "allow-redis-${var.ENV}"
+#   }
+# }
 
-connection {
-  host = aws_instance.redis.public_ip
-  type = "ssh"
-  #private_key = file("${local.key_path}") read private_key (PEM) from file
-  user = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_USER"]
-  password = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_PASS"]
+output "redis" {
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
 
-provisioner "remote-exec" {
-  inline = [ 
-    "set-hostname ${var.COMPONENT}",
-    "yum install epel-release -y",
-    "yum install yum-utils -y",
-    "yum install ${var.REDIS_REPO} -y",
-    "yum-config-manager --enable remi",
-    "yum install redis -y",
-     ]
-}
-
-provisioner "local-exec" {
-  command = "echo ${aws_instance.redis.public_ip} > redis_inv"
-  #command = "ansible-playbook -i ${aws_instance.redis.public_ip}, --private-key ${local.key_path} ${var.COMPONENT}.yml"
-  #echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv
-}
-}
-
-resource "aws_route53_record" "jithendar" {
-  name          = "${var.COMPONENT}.${data.aws_route53_zone.jithendar.name}"
-  type          = "A"
-  ttl           = "300"
-  zone_id       = data.aws_route53_zone.jithendar.zone_id
-  records       = [aws_instance.redis.public_ip]
+resource "aws_route53_record" "redis" {
+  name        = "redis-${var.ENV}"
+  type        = "CNAME"
+  zone_id     = data.terraform_remote_state.vpc.outputs.ZONE_ID
+  ttl         = "1000"
+  records     = [aws_elasticache_cluster.redis.cache_nodes[0].address] //endpoint wont be der..so using cache nodes... and cache node will have id, address, port and availability_zone information
 }
