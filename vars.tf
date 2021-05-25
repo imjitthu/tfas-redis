82d4f36fd99cf4b218f5cadd8d78d87bc3e64fae
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 09:03:33 +0530

    roboshop

--- vars.tf
+++ vars.tf
@@ -8,8 +8,7 @@ variable "DOMAIN" {default = ""}
 
 variable "COMPONENT" {}
 variable "AMI" {}
-variable "PASSWORD" {}
-variable "USER" {}
+
 variable "REDIS_REPO" {
     default = "http://rpms.remirepo.net/enterprise/remi-release-7.rpm"
 }
